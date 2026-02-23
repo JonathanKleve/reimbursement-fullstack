20909ea053e50b6672dbea23791de4f596a455bb
JonathanKleve <194426067+JonathanKleve@users.noreply.github.com>
2026-02-23 11:49:52 -0500

    initial setup and commit
    # Conflicts:
    #	.gitignore

---
+++ src/test/java/io/github/jonathankleve/reimbursementservice/ReimbursementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonathankleve.reimbursementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
